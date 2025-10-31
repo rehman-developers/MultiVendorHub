fda2a695fca7bf68134602d4944a8595fb786d99
Abdul Rehman <167124505+rehman-developers@users.noreply.github.com>
2025-10-31 10:10:02 +0500

    Delete MultiVendorHub directory

--- MultiVendorHub/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductApiController;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/products', [ProductApiController::class, 'index']);
-    // Add more API routes here
-});
